811dee1b7262ecdbc7e6844636db4cb4cd40af78
StefNunes <2022255@student.cct.ie>
2023-11-29 10:30:26 +0000

    Classes
    those are the classes that I expect to use for this project

---
+++ src/prototype_software/Regular_User.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package prototype_software;
+
+/**
+ *
+ * @author Stefani Nunes
+ */
+public class Regular_User {
+    
+}
